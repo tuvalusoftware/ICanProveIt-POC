56046081ddd794596045e16bea188068463e9695
Kim Minh Thang <thangved02@gmail.com>
2024-01-05 11:33:20 +0700

    feat: update app ui

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,5 @@
 import { App as AntApp, ConfigProvider } from 'antd';
+import 'antd/dist/reset.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
