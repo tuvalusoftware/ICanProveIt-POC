fd948bac8d3b7c17b266605c77eed695df72fc4d
Kim Minh Thang <thangved02@gmail.com>
2024-01-10 14:35:18 +0700

    feat: add helpers

--- api/helpers.py
+++ api/helpers.py
@@ -1,4 +1,2 @@
 def pretty_docs(docs):
-    return f"\n{'-' * 100}\n".join(
-        [f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)]
-        )
+    return f"\n{'-' * 100}\n".join([f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)])
