2090a3fbd799fe787f12c22105dd25875d7ac182
Kim Minh Thang <thangved02@gmail.com>
2024-01-24 14:48:11 +0700

    chore: add ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+        {
+            name: "api",
+            script: "poetry run uvicorn main:app --host 0.0.0.0 --port 8080",
+            cwd: "./api-v2",
+        },
+        {
+            name: "ui",
+            script: "npx serve -s dist -l 3000",
+            cwd: "./client-v2",
+        },
+    ],
+};
